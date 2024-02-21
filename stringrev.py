3ad786dc127793778bac3e7b47fa0e7a31e9c092
Soochak S Kotyan <sundarasulochanasoochi@gmail.com>
2024-02-21 11:22:21 +0000

    First commit

---
+++ stringrev.py
@@ -0,0 +1,6 @@
+def stringRev():
+    InputString = input("Enter the string to be reversed: ")
+    ReversedString = InputString[::-1]
+    print(f"The reversed string is: {ReversedString}")
+
+stringRev()
