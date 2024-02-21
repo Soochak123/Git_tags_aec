ea6821bce167f78b5ac3c399d4cf8ff397900f96
Soochak S Kotyan <sundarasulochanasoochi@gmail.com>
2024-02-21 11:26:05 +0000

    Second commit

---
+++ strconcat.py
@@ -0,0 +1,6 @@
+def stringConcat():
+    str1 = input("Enter the first string: ")
+    str2 = input("Enter the second string: ")
+    print("The concatenated string is: ", str1 + str2)
+
+stringConcat()
